d44b4dfbc75887338dccc508b2a533c2990667cc
Justin Newberry <jnewberry0502@gmail.com>
2023-06-13 09:22:42 -0400

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,11 @@
 FROM ubuntu:20.04
 
+ADD rsync.sh /data/rsync.sh
+RUN CHMOD +x /data/rsync.sh
+
+ADD cron /etc/cron.d/rsync
+RUN chmod 0644 /etc/cron.d/rsync
+
+RUN crontab /etc/cron.d/rsync
+
+ENTRYPOINT ["cron", "-f"]
--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,6 @@
-0 * * * * ssh-keyscan comma-d9df6f87e8feff94 >> ~/.ssh/known_hosts
-5 * * * * rsync -og --chown=99:100 --chmod=ugo=rwX -r comma@10.0.0.37:/data/media/0/realdata/* /mnt/routes
-10 * * * * rsync -og --chown=99:100 --chmod=ugo=rwX -r comma-d9df6f87e8feff94:/data/media/0/realdata/* /mnt/routes
+#!/bin/sh
+
+ssh-keyscan comma-d9df6f87e8feff94 >> ~/.ssh/known_hosts
+
+rsync -og --chown=99:100 --chmod=ugo=rwX -r comma@10.0.0.37:/data/media/0/realdata/* /mnt/routes
+rsync -og --chown=99:100 --chmod=ugo=rwX -r comma-d9df6f87e8feff94:/data/media/0/realdata/* /mnt/routes
