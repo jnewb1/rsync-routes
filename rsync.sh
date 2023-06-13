6fc5812b8e8c2f995e757b7a9138712062f515a1
Justin Newberry <jnewberry0502@gmail.com>
2023-06-13 17:51:45 -0400

    show progress

--- rsync.sh
+++ rsync.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo "Running rsync..."
+
 ssh-keyscan ssh.comma.ai >> ~/.ssh/known_hosts
 
-rsync -og --chown=99:100 --chmod=ugo=rwX -r comma@10.0.0.37:/data/media/0/realdata/* /mnt/routes
-rsync -og --chown=99:100 --chmod=ugo=rwX -r comma-d9df6f87e8feff94:/data/media/0/realdata/* /mnt/routes
+rsync --progress -og --chown=99:100 --chmod=ugo=rwX -r comma@10.0.0.37:/data/media/0/realdata/* /mnt/routes
+rsync --progress -og --chown=99:100 --chmod=ugo=rwX -r comma-d9df6f87e8feff94:/data/media/0/realdata/* /mnt/routes
