d501704a9377c394ac4caa0155a40fb4d20cdf8f
Justin Newberry <jnewberry0502@gmail.com>
2023-06-13 10:07:54 -0400

    wip

--- rsync.sh
+++ rsync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh-keyscan comma-d9df6f87e8feff94 >> ~/.ssh/known_hosts
+ssh-keyscan ssh.comma.ai >> ~/.ssh/known_hosts
 
 rsync -og --chown=99:100 --chmod=ugo=rwX -r comma@10.0.0.37:/data/media/0/realdata/* /mnt/routes
 rsync -og --chown=99:100 --chmod=ugo=rwX -r comma-d9df6f87e8feff94:/data/media/0/realdata/* /mnt/routes
