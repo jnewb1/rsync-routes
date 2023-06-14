55ecd1fdf8ed4f71f82ec61b8cd490e57bd74f3a
Justin Newberry <jnewberry0502@gmail.com>
2023-06-14 00:42:03 -0400

    remote that

--- rsync.sh
+++ rsync.sh
@@ -11,5 +11,5 @@ ssh-keyscan ssh.comma.ai >> ~/.ssh/known_hosts
 
 RSYNC_CMD="rsync --bwlimit=5000 --progress -og --chown=99:100 --chmod=ugo=rwX"
 
-$RSYNC_CMD -r comma@10.0.90.37:/data/media/0/realdata/* /mnt/routes
-$RSYNC_CMD -r comma-d9df6f87e8feff94:/data/media/0/realdata/* /mnt/routes
+$RSYNC_CMD -r comma@10.0.90.37:/data/media/0/realdata /mnt/routes
+$RSYNC_CMD -r comma-d9df6f87e8feff94:/data/media/0/realdata /mnt/routes
