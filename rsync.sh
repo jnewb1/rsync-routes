64e30cda2fe54e6f50add0e02047f037651c0cc4
Justin Newberry <jnewberry0502@gmail.com>
2023-06-14 10:02:16 -0400

    size only because of how OP works

--- rsync.sh
+++ rsync.sh
@@ -10,7 +10,7 @@ echo "Running rsync..."
 ssh-keyscan "10.0.90.37" >> ~/.ssh/known_hosts
 ssh-keyscan "ssh.comma.ai" >> ~/.ssh/known_hosts
 
-RSYNC_CMD="rsync --bwlimit=5000 --progress -og --chown=99:100 --chmod=ugo=rwX"
+RSYNC_CMD="rsync --size-only --bwlimit=5000 --progress -og --chown=99:100 --chmod=ugo=rwX"
 
 $RSYNC_CMD -r comma@10.0.90.37:/data/media/0/realdata/ /mnt/routes/d9df6f87e8feff94/
 $RSYNC_CMD -r comma-d9df6f87e8feff94:/data/media/0/realdata/ /mnt/routes/d9df6f87e8feff94/
