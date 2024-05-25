8884a20a3a34bd21e59523c0b1b9ae5ff271be62
Justin Newberry <jnewberry0502@gmail.com>
2024-05-24 22:15:03 -0400

    make this easier

--- rsync.sh
+++ rsync.sh
@@ -10,7 +10,7 @@ echo $$ > "$pid"
 echo "Running rsync..."
 
 SSH_ARGS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-RSYNC_CMD="rsync --size-only --bwlimit=5000 --progress -og --chown=99:100 --chmod=ugo=rwX -e 'ssh $SSH_ARGS'"
+RSYNC_CMD="rsync --size-only --bwlimit=5000 --progress -og --chown=99:100 --chmod=ugo=rwX"
 
 HOSTS="192.168.1.70,192.168.1.142"
 
@@ -19,5 +19,5 @@ do
     DONGLE_ID="$(ssh $SSH_ARGS comma@$host 'cat /data/params/d/DongleId')"
 
     echo "rsyncing from comma@$host with dongle id $DONGLE_ID"
-    $RSYNC_CMD -r comma@$host:/data/media/0/realdata/ /mnt/routes/$DONGLE_ID/
+    $RSYNC_CMD -e "ssh $SSH_ARGS" -r comma@$host:/data/media/0/realdata/ /mnt/routes/$DONGLE_ID/
 done
